de84a7eed4a350031f0a8faa282118bf6faa5f96
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-24 22:01:56 +0400

    more helpers

---
+++ src/Charsets.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/charset-validator" package.
+ * https://github.com/charcoal-dev/charset-validator
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/charset-validator/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Charsets;
+
+/**
+ * Class Charsets
+ * @package Charcoal\Charsets
+ */
+class Charsets
+{
+    /**
+     * Cleans input string of any leading/trailing spaces,
+     * Also replaces multiple-spaces in between string with a single space
+     * @param string $input
+     * @return string
+     */
+    public static function cleanSpaces(string $input): string
+    {
+        return trim(preg_replace('/(\s+)/', ' ', $input));
+    }
+
+    /**
+     * Checks is input string all comprised of 7-bit ASCII characters
+     * @param $val
+     * @param bool $printableOnly
+     * @return bool
+     */
+    public static function isASCII($val, bool $printableOnly = true): bool
+    {
+        if (!is_string($val)) {
+            return false;
+        }
+
+        return $printableOnly ? ASCII::isPrintableOnly($val) : ASCII::Charset($val);
+    }
+
+    /**
+     * Checks if string may have UTF8 characters
+     * @param $val
+     * @return bool
+     */
+    public static function hasUtf8Chars($val): bool
+    {
+        return is_string($val) && !ASCII::Charset($val);
+    }
+
+    /**
+     * Checks if input is a string and is Hexadecimal encoded
+     * @param $val
+     * @return bool
+     */
+    public static function isBase16($val): bool
+    {
+        return is_string($val) && preg_match('/^(0x)?[a-f0-9]+$/i', $val);
+    }
+}
